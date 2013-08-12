86cd3cc897f2d8cbd467ffbeb5a2fcba5a1673e1
Gustavo De Souza <gusthavo.souzapereira@gmail.com>
2013-08-12 08:30:30 -0300

    Update Calculadora.java

--- Calc/src/Calculadora/Calculadora.java
+++ Calc/src/Calculadora/Calculadora.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Calculadora;
+ package Calc.src.Calculadora;
 
 
 import java.awt.event.KeyEvent;
